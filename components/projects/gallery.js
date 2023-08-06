3319fd1ac2a3125641c1ccf71beb5c94d38dedbd
mivaniv <kol9nivaniv@gmail.com>
2023-08-06 16:30:13 +0300

    fix

---
+++ components/projects/gallery.js
@@ -0,0 +1,41 @@
+
+function createModal() {
+    document.querySelector('body').insertAdjacentHTML
+    ('beforeend',   ` <div class="modalContainer" aria-modal="true" role="dialog">
+            <div class="modal">
+                <div class="modalOverlay">
+                    <div class="modalBtnContainer">
+                        <button class="modalBtn modalArrow modalArrowLeft" id="left" aria-label="Previous
+                        image">
+                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.25 6.75L4.75 12L10.25 17.25"></path>
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.25 12H5"></path>
+                            </svg>
+                        </button>
+                        <button class="modalBtn modalClose"  aria-label="Close Gallery">
+                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 6.75L6.75 17.25"></path>
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75L17.25 17.25"></path>
+                            </svg>
+                        </button>
+                        <button class="modalBtn modalArrow modalArrowRight" id="right" aria-label="Next image">
+                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13.75 6.75L19.25 12L13.75 17.25"></path>
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 12H4.75"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="modalIndicatorContainer">
+                        <button class="modalIndicator">
+
+                        </button>
+                    </div>
+                </div>
+                <div class="modalImageContainer">
+                    <img src="../img/proj1/img%20(1).jpg" alt="Test" class="modalImage"/>
+                </div>
+            </div>
+        </div>
+    `)
+}
+createModal();
---
+++ components/projects/script.js
@@ -0,0 +1,44 @@
+const zoomBtn = document.querySelectorAll('.projectsGalleryProjectInformationZoom');
+const allImages = document.querySelectorAll('.projectsGalleryProject');
+const imgView = document.querySelector('.imageView');
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+const imgBox = document.querySelector('.imageBox');
+
+let currentImageIdx = 1;
+
+imgView.addEventListener('click', function () {
+    this.style.display = "none";
+    imgBox.style.display = "none";
+})
+
+
+zoomBtn.forEach(function (btn, index) {
+    btn.addEventListener('click', function () {
+        imgView.style.display = "block";
+        imgBox.style.display = "block";
+        currentImageIdx = index + 1;
+        currentImageDisplay(currentImageIdx);
+    })
+})
+
+function currentImageDisplay(position) {
+    imgBox.style.background = `url(components/img/proj1/img (${currentImageIdx}).jpg) center/cover no-repeat`;
+}
+
+prevBtn.addEventListener('click', function () {
+    currentImageIdx--;
+    if (currentImageIdx === 0) {
+        currentImageIdx = allImages.length;
+    }
+    currentImageDisplay(currentImageIdx);
+})
+
+nextBtn.addEventListener('click', function () {
+    currentImageIdx++;
+    if (currentImageIdx === 5) {
+        currentImageIdx = 1;
+    }
+    currentImageDisplay(currentImageIdx);
+})
+
